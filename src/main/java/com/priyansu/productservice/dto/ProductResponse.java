c2c0aa131aaa5f2260f3f0884678bd5fa96a8b9b
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-17 00:37:03 +0530

    added: ProductResponse (DTO)

---
+++ src/main/java/com/priyansu/productservice/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package com.priyansu.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Priyansu Sahoo
+ * 17-04-2024
+ * @Project product-service
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponse {
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
