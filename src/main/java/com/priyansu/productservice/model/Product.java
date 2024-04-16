da44c97c683e42b2e8fa33e780f2c57953de3283
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-17 00:33:57 +0530

    added "product" entity

---
+++ src/main/java/com/priyansu/productservice/model/Product.java
@@ -0,0 +1,25 @@
+package com.priyansu.productservice.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Priyansu Sahoo
+ * 16-04-2024
+ * @Project product-service
+ */
+@Document(value = "product")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
