5d5cfe39ca47d5af932b35659b6ecb650ab5eff6
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-17 23:52:35 +0530

    Optimized: ProductService

--- src/main/java/com/priyansu/productservice/service/ProductService.java
+++ src/main/java/com/priyansu/productservice/service/ProductService.java
@@ -5,7 +5,11 @@ import com.priyansu.productservice.model.Product;
 import com.priyansu.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * @author Priyansu Sahoo
@@ -19,19 +23,30 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
-    public void createProduct(ProductRequest productRequest) {
+    public ResponseEntity<Boolean> createProduct(ProductRequest productRequest) {
         try {
-            Product newProduct = Product.builder()
-                    .name(productRequest.getName())
-                    .description(productRequest.getDescription())
-                    .price(productRequest.getPrice())
-                    .build();
+            if (productRequest.getName() == null || productRequest.getName().isEmpty() ||
+                    productRequest.getDescription() == null || productRequest.getDescription().isEmpty() ||
+                    productRequest.getPrice() == null ){
 
-            productRepository.save(newProduct);
+                log.info("Getting parameter(s) null or empty");
 
-            log.info("New product with id {} created", newProduct.getId());
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Boolean.FALSE);
+
+            } else{
+                Product newProduct = Product.builder()
+                        .name(productRequest.getName())
+                        .description(productRequest.getDescription())
+                        .price(productRequest.getPrice())
+                        .build();
+
+                productRepository.save(newProduct);
+                log.info("New product with id {} is created", newProduct.getId());
+                return ResponseEntity.status(HttpStatus.CREATED).body(Boolean.TRUE);
+            }
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+        return null;
     }
 }
