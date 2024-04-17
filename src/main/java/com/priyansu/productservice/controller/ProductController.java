09c80990b00e34a770cd3916c23de8fa39338f64
Priyansu Sahoo <priyansusahoo1@gmail.com>
2024-04-17 23:52:10 +0530

    Modified: ProductController

--- src/main/java/com/priyansu/productservice/controller/ProductController.java
+++ src/main/java/com/priyansu/productservice/controller/ProductController.java
@@ -4,8 +4,6 @@ import com.priyansu.productservice.dto.ProductRequest;
 import com.priyansu.productservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +24,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("createProduct")
-    public ResponseEntity<ProductRequest> createProduct(@RequestBody ProductRequest productRequest){
-        try {
-            productService.createProduct(productRequest);
-            return ResponseEntity.status(HttpStatus.CREATED).body(productRequest);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(productRequest);
-        }
+    public ResponseEntity<Boolean> createProduct(@RequestBody ProductRequest productRequest){
+            return productService.createProduct(productRequest);
     }
 }
